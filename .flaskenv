042c070f6d146a5f87c67a5f257f24ff3f792176
artur <asihdi@gmail.com>
2020-09-23 20:10:14 +0200

    heroku deployment changes

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
+# python-dotenv automatically set environment variables defined below:
 FLASK_APP=eTracker
--- config.py
+++ config.py
@@ -34,7 +34,7 @@ class Config(object):
     # Email Service:
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = 'pythonarts@op.pl'
